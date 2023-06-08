6f0d1dc87737aa83dc3935e9ebeb82851e930498
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2023-06-08 17:37:26 -0300

    refactor cards layout

--- src/infra/router/Router.tsx
+++ src/infra/router/Router.tsx
@@ -4,18 +4,27 @@ import Layout from '../../application/pages/Layout/Layout';
 import TwoPlayers from '../../application/pages/twoPlayers/TwoPlayers';
 
 const Router = (): JSX.Element => {
+    const routes = [
+        {
+            path: '/two-players',
+            element: <TwoPlayers />,
+        },
+    ];
+
+    const mountRoutes = () => {
+        return routes.map((route) => (
+            <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+            />
+        ));
+    };
+
     return (
         <BrowserRouter>
             <Routes>
-                <Route
-                    path='/'
-                    element={<Layout />}
-                >
-                    <Route
-                        element={<TwoPlayers />}
-                        path='two-players'
-                    />
-                </Route>
+                <Route element={<Layout />}>{mountRoutes()}</Route>
             </Routes>
         </BrowserRouter>
     );
